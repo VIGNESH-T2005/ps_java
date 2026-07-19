93ab9ad704f55ae14f0bd18a9154429649867e4e
VIGNESH T <vigneshhari8739@gmail.com>
2026-07-19 21:04:59 +0530

    perfect no

--- src/num_bas_pblm/perfect_number.java
+++ src/num_bas_pblm/perfect_number.java
@@ -1,9 +1,23 @@
 package num_bas_pblm;
 
 public class perfect_number {
+	public static void perfect(int n) {
+		int sum=0;
+		for (int i = 1; i<n;i++) {
+			if(n%i==0) {
+				sum+=i;
+			}
+		}
+		if(sum==n)
+			System.out.println("perfect number");
+		else
+			System.out.println("not perfect");
+		
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		perfect(6);
 
 	}
 
